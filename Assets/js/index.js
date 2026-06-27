395ac3c7dc889384b4970ecb482aed14b52299a8
ElDixer <biringaninejoseph@gmail.com>
2026-06-27 15:48:43 +0200

    adding cross and correcting burger

--- Assets/js/index.js
+++ Assets/js/index.js
@@ -1,11 +1,18 @@
 const burger = document.querySelector('#burgermenu');
 const menu = document.querySelector('#menulist');
+const cross = document.querySelector('#crossmenu');
 
 burger.addEventListener('click', () => {
     if (menu.classList.contains('hidden')) {
         menu.classList.remove('hidden');
         burger.classList.add('hidden');
+        cross.classList.remove('hidden');
     } else {
-        menu.classList.add('hidden');
+       menu.classList.add('hidden');
     }
+})
+cross.addEventListener('click', () => {
+    menu.classList.add('hidden');
+    burger.classList.remove('hidden');
+    cross.classList.add('hidden');
 })
