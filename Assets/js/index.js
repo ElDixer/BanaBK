45575c9f78346a9ebe3249c5ff77775ff9f8c498
ElDixer <biringaninejoseph@gmail.com>
2026-06-27 16:19:21 +0200

    changing js functions

--- Assets/js/index.js
+++ Assets/js/index.js
@@ -2,17 +2,21 @@ const burger = document.querySelector('#burgermenu');
 const menu = document.querySelector('#menulist');
 const cross = document.querySelector('#crossmenu');
 
-burger.addEventListener('click', () => {
-    if (menu.classList.contains('hidden')) {
-        menu.classList.remove('hidden');
-        burger.classList.add('hidden');
-        cross.classList.remove('hidden');
-    } else {
-       menu.classList.add('hidden');
-    }
-})
-cross.addEventListener('click', () => {
+function openmenu() {
+    menu.classList.remove('hidden');
+    burger.classList.add('hidden');
+    cross.classList.remove('hidden');
+}
+function closemenu() {
     menu.classList.add('hidden');
     burger.classList.remove('hidden');
     cross.classList.add('hidden');
+}
+
+burger.addEventListener("click", () =>{
+    openmenu();
+})
+
+cross.addEventListener("click", () => {
+    closemenu();
 })
