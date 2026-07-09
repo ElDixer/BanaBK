64dd0442e1a1a1428116c6c7e242166d93240664
ElDixer <biringaninejoseph@gmail.com>
2026-07-09 14:30:00 +0200

    adding product js page

--- Assets/js/index.js
+++ Assets/js/index.js
@@ -2,19 +2,11 @@ const burger = document.querySelector('#burgermenu');
 const menu = document.querySelector('#menulist');
 const cross = document.querySelector('#crossmenu');
 const activepage = window.location.pathname.split("/").pop() || "index.html";
-const mainimage = document.getElementById("mainimage");
-const imagecarousel = document.querySelectorAll(".carousel");
-let currentindex = 0;
-const input = document.getElementById("input");
-const moins = document.getElementById("moins");
-const plus = document.getElementById("plus");
-const payerBtn = document.getElementById("payerBtn");
 
 
 function openmenu() {
     menu.classList.remove('hidden');
     burger.classList.add('hidden');
-    // menu.classList.add('block');
     cross.classList.remove('hidden');
 }
 function closemenu() {
@@ -55,58 +47,3 @@ document.querySelectorAll(".navlink").forEach(link => {
     }
 })
 
-imagecarousel.forEach(carousel => {
-    carousel.addEventListener("click", () => {
-        mainimage.src = carousel.src;
-    })
-})
-
-setInterval(() => {
-    currentindex++;
-    if (currentindex >= imagecarousel.length) {
-        currentindex = 0;
-    }
-    mainimage.src = imagecarousel[currentindex].src;
-}, 2000);
-
-plus.addEventListener("click", () =>{
-    input.value = Number(input.value) + 1;
-})
-
-moins.addEventListener("click", () =>{
-    if (Number(input.value) > 1) {
-        input.value = Number(input.value) - 1;
-    } else {
-        input.value = 0;
-    }
-})
-
-payerBtn.addEventListener("click", () => {
-
-    const produit = "HOODIE COLLECTION";
-    const prix = 15;
-
-    const quantite = document.getElementById("input").value || 1;
-
-    const taille = document.querySelectorAll(".size").forEach(size => {
-        size.addEventListener("click", () => {
-            taille = size.innerText;
-        });
-    });
-
-    const numeroWhatsApp = "243904550059"; // Remplace par ton numéro avec indicatif pays
-
-    const message = `Bonjour, je souhaite acheter :
-
-    
-Produit : ${produit}
-Prix : ${prix}$
-Quantité : ${quantite}
-Taille : ${taille}
-
-Merci.`;
-
-    const url = `https://wa.me/${numeroWhatsApp}?text=${encodeURIComponent(message)}`;
-
-    window.open(url, "_blank");
-});
---
+++ Assets/js/product.js
@@ -0,0 +1,63 @@
+const mainimage = document.getElementById("mainimage");
+const imagecarousel = document.querySelectorAll(".carousel");
+const input = document.getElementById("input");
+const moins = document.getElementById("moins");
+const plus = document.getElementById("plus");
+const payerBtn = document.getElementById("payerBtn");
+let currentindex = 0;
+
+imagecarousel.forEach(carousel => {
+    carousel.addEventListener("click", () => {
+        mainimage.src = carousel.src;
+    })
+})
+
+setInterval(() => {
+    currentindex++;
+    if (currentindex >= imagecarousel.length) {
+        currentindex = 0;
+    }
+    mainimage.src = imagecarousel[currentindex].src;
+}, 2000);
+
+plus.addEventListener("click", () =>{
+    input.value = Number(input.value) + 1;
+})
+
+moins.addEventListener("click", () =>{
+    if (Number(input.value) > 1) {
+        input.value = Number(input.value) - 1;
+    } else {
+        input.value = 0;
+    }
+})
+
+payerBtn.addEventListener("click", () => {
+
+    const produit = "HOODIE COLLECTION";
+    const prix = 15;
+
+    const quantite = document.getElementById("input").value || 1;
+
+    let taille = document.querySelectorAll(".size").forEach(size => {
+        size.addEventListener("click", () => {
+            taille = size.innerText;
+        }); 
+    });
+
+    const numeroWhatsApp = "243904550059";
+
+    const message = `Bonjour, je souhaite acheter :
+
+    
+Produit : ${produit}
+Prix : ${prix}$
+Quantité : ${quantite}
+Taille : ${taille}
+
+Merci.`;
+
+    const url = `https://wa.me/${numeroWhatsApp}?text=${encodeURIComponent(message)}`;
+
+    window.open(url, "_blank");
+});
