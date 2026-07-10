704ec7d8be4ec50a3d3777cc9277ae0a3a83b175
ElDixer <biringaninejoseph@gmail.com>
2026-07-10 14:39:33 +0200

    changing product page

--- Assets/js/product.js
+++ Assets/js/product.js
@@ -4,6 +4,9 @@ const input = document.getElementById("input");
 const moins = document.getElementById("moins");
 const plus = document.getElementById("plus");
 const payerBtn = document.getElementById("payerBtn");
+const params = new URLSearchParams(window.location.search);
+const id = Number(params.get("id"));
+
 
 
 imagecarousel.forEach(carousel => {
@@ -60,4 +63,97 @@ Merci.`;
     const url = `https://wa.me/${numeroWhatsApp}?text=${encodeURIComponent(message)}`;
 
     window.open(url, "_blank");
+});
+
+const products = [
+    {
+        id: 1,
+        name: "BatotoYaBk Tshirt Noir",
+        price: "$15",
+        material: "Premium Cotton",
+        image: "./Assets/images/batoto ya bk black.png",
+        category: "hoodie"
+    },
+
+    {
+        id: 2,
+        name: "BatotoYaBk tshirt Navy",
+        price: "$15",
+        material: "Premium Cotton",
+        image: "./Assets/images/batoto ya bk color.png",
+        category: "tshirt"
+    },
+
+    {
+        id: 3,
+        name: "BatotoYaBk tshirt rouge",
+        price: "$15",
+        material: "Premium Cotton",
+        image: "./Assets/images/batoto ya bk red.png",
+        category: "tshirt"
+    },
+
+    {
+        id: 4,
+        name: "BatotoYaBk tshirt blanc",
+        price: "$15",
+        material: "Premium Cotton",
+        image: "./Assets/images/batoto ya bk white.png",
+        category: "Tshirt"
+    }
+];
+
+const product = products.find(item => item.id === id);
+
+document.getElementById("productImage").src = product.image;
+document.getElementById("productName").textContent = product.name;
+document.getElementById("productPrice").textContent = product.price;
+
+
+
+const search = document.getElementById("searchInput");
+
+search.addEventListener("input", function () {
+
+    const keyword = this.value.toLowerCase();
+
+    document.querySelectorAll(".product-card").forEach(card => {
+
+        const title = card.querySelector("h3").textContent.toLowerCase();
+
+        if (title.includes(keyword)) {
+            card.style.display = "block";
+        } else {
+            card.style.display = "none";
+        }
+
+    });
+
+});
+
+
+
+const buttons = document.querySelectorAll("[data-category]");
+
+buttons.forEach(button => {
+
+    button.addEventListener("click", () => {
+
+        const category = button.dataset.category;
+
+        document.querySelectorAll(".product-card").forEach(card => {
+
+            if (
+                category === "all" ||
+                card.dataset.category === category
+            ) {
+                card.style.display = "block";
+            } else {
+                card.style.display = "none";
+            }
+
+        });
+
+    });
+
 });
